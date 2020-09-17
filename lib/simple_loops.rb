49571f9a5bb07a7b0ce2398d3e6152104521f267
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-09-17 19:00:13 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -4,8 +4,10 @@ def loop_message_five_times(string)
   end
 end
 
-def loop_message_n_times
-  
+def loop_message_n_times(string, num)
+  num.times do
+    puts string
+  end
 end
 
 def output_array
